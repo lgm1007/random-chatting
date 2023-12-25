6582f75f8aa83dc47f8b0c30e420ae5480aedd02
lgm1007 <flxl654@gmail.com>
2023-12-25 12:57:03 +0900

    view: 클라이언트단 작업 - socket.io 라이브러리 등록 및 밑바닥 작업

--- publics/js/scripts.js
+++ publics/js/scripts.js
@@ -1,1 +1,16 @@
-alert('hello world');
+const socket = io('/');
+const getElementById = (id) => document.getElementById(id) || null;
+
+const helloStrangerElement = getElementById('hello_stranger');
+const chattingBoxElement = getElementById('chatting_box');
+const formElement = getElementById('chat_form');
+
+function helloUser() {
+  const userName = prompt('What is your name?');
+}
+
+function init() {
+  helloUser();
+}
+
+init();
