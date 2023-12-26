b18dca3a58aed7fde2b2bf1f8fab9575efc67f69
lgm1007 <flxl654@gmail.com>
2023-12-26 11:29:40 +0900

    feat: 게이트웨이 네임스페이스 및 생명주기 인터페이스 구현

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,13 +1,33 @@
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`connected: ${socket.id} ${socket.nsp.name}`);
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`disconnected: ${socket.id} ${socket.nsp.name}`);
+  }
+
+  afterInit() {
+    this.logger.log('init');
+  }
+
   @SubscribeMessage('new_user')
   handleNewUserEvent(
     @MessageBody() userName: string,
