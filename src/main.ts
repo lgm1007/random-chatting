3d8241f0958e3b337600ba72cea4b62f9057f877
lgm1007 <flxl654@gmail.com>
2023-12-26 12:40:20 +0900

    feat: class-validator 사용을 위한 globalPipes 등록

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import * as process from 'process';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'publics'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+  app.useGlobalPipes(new ValidationPipe());
   const PORT = process.env.PORT;
   await app.listen(PORT);
 }
